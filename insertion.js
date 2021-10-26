4034a37f230ede5f691a6c548c1d42f35563b5fa
Adam Thometz <adamthometz@gmail.com>
2021-10-26 09:04:27 -0400

    further study: insertion sort

--- insertion.js
+++ insertion.js
@@ -1,3 +1,16 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  if (arr.length <= 1) return arr
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        const temp = arr.splice(i, 1)
+        arr.splice(j, 0, temp[0])
+      }
+    }
+  }
+  return arr
+}
 
 module.exports = insertionSort;
+
+// best for nearly sorted data
