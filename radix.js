dc095ed1824bcc31e9c9f84c531dfc455e539d16
Adam Thometz <adamthometz@gmail.com>
2021-10-26 16:24:36 -0400

    further study: radix sort

--- radix.js
+++ radix.js
@@ -1,3 +1,38 @@
-function radixSort() {}
+// HELPERS
+function getDigit(num, idx) {
+  const divided = Math.abs(num) / Math.pow(10, idx)
+  const floored = Math.floor(divided)
+  const digit = floored % 10
 
-module.exports = radixSort;
+  return digit
+}
+function digitCount(num) {
+  if (num === 0) return 1
+  const power = Math.log10(Math.abs(num))
+  const floored = Math.floor(power)
+  return floored + 1
+}
+function mostDigits(nums) {
+  let maxDigits = 0
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
+
+// MAIN FUNCTION
+function radixSort(nums) {
+  let maxDigits = mostDigits(nums)
+
+  for (let i = 0; i < maxDigits; i++) {
+    let digitBuckets = Array.from({length: 10}, () => [])
+    for (let j = 0; j < nums.length; j++) {
+      let digitized = getDigit(nums[j], i)
+      digitBuckets[digitized].push(nums[j])
+    }
+    nums = [].concat(...digitBuckets)
+  }
+  return nums
+}
+
+module.exports = {getDigit, digitCount, mostDigits, radixSort};
