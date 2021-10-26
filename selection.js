962c680e43a3da7d46bedf3bdc884d81ee8a9083
Adam Thometz <adamthometz@gmail.com>
2021-10-26 11:17:03 -0400

    selection sort: slight refactor

--- selection.js
+++ selection.js
@@ -1,18 +1,18 @@
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let min = arr[i]
-    let minIdx = i
+    let min = arr[i];
+    let minIdx = i;
     for (let j = i+1; j < arr.length; j++) {
       if (arr[j] < min) {
-        min = arr[j]
-        minIdx = j
+        min = arr[j];
+        minIdx = j;
       }
     }
-    let temp = arr[minIdx]
-    arr[minIdx] = arr[i]
-    arr[i] = temp
+    let temp = arr[minIdx];
+    arr[minIdx] = arr[i];
+    arr[i] = temp;
   }
-  return arr
-}
+  return arr;
+};
 
 module.exports = selectionSort;
