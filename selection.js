55ca6745229c67df232e7049c4b06c364942519a
Adam Thometz <adamthometz@gmail.com>
2021-10-26 11:07:55 -0400

    insertion sort: slight refactor

--- selection.js
+++ selection.js
@@ -1,22 +1,16 @@
 function selectionSort(arr) {
-  if (arr.length <= 1) return arr
   for (let i = 0; i < arr.length; i++) {
     let min = arr[i]
     let minIdx = i
-    let swapped = false
     for (let j = i+1; j < arr.length; j++) {
       if (arr[j] < min) {
         min = arr[j]
         minIdx = j
-        swapped = true
       }
     }
-    if (swapped) {
-      let temp = arr[minIdx]
-      arr[minIdx] = arr[i]
-      arr[i] = temp
-      
-    }
+    let temp = arr[minIdx]
+    arr[minIdx] = arr[i]
+    arr[i] = temp
   }
   return arr
 }
